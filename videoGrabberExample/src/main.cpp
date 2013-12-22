40c5b3c51ba0e8e20b578999703a449b3d458c78
sabotai <sabotaione@gmail.com>
2013-12-22 00:52:34 -0500

    Update main.cpp

--- videoGrabberExample/src/main.cpp
+++ videoGrabberExample/src/main.cpp
@@ -1,14 +1,26 @@
 #include "ofMain.h"
+//#include "ofApp.h"
+//#include "ofAppNoWindow.h"
 #include "testApp.h"
+#include <stdio.h> 
+
+extern "C" {
+#include <wiringPi.h>
+}  
 
 //========================================================================
 int main( ){
 
-	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
+if (wiringPiSetup() == -1)
+return 1 ;
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp( new testApp());
+
+//ofAppNoWindow w;
+ofSetupOpenGL(160,120, OF_WINDOW);	 // <-------- setup the GL context
+
+// this kicks off the running of my app
+// can be OF_WINDOW or OF_FULLSCREEN
+// pass in width and height too:
+ofRunApp( new testApp());
 
 }
