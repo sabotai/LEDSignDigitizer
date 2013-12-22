3c85365f4aa41d8928403771b6ded5d2f7f68042
sabotai <sabotaione@gmail.com>
2013-12-22 00:52:03 -0500

    Update ledHashTable.h

--- videoGrabberExample/src/ledHashTable.h
+++ videoGrabberExample/src/ledHashTable.h
@@ -1,29 +1,22 @@
+// This tells the code the order in which to send the pixels.  the first 4 numbers in each array are the locations (within the total 512) 
+// of the pixels in the first "column".  The actual rows in the sign alternate between the 4, so the cartesian order would be ---------
+// row0, row1, row2, row3, row0, row1, row2, etc.. After the first 4 pixels in the row, the next 4 locations are 8 pixels over (since we're 
+// clocking in 8 pixels in each chunk.
 
-// LED Display
-#define LED_NO_OF_PANELS	8
-#define LED_PANEL_WIDTH         32
-#define LED_PANEL_HEIGHT        8
-#define LED_DISPLAY_WIDTH	128
-#define LED_DISPLAY_HEIGHT	32
 
-//0, 128, 256, 384, 8, 136, 264, 392, 16, 144, 272, 400, 24, 152, 280, 408 //row 0
-//32, 160, 288, 416,40, 168, 296, 424, 48, 176, 304, 432, 56, 184, 312, 440 //row 1
-//64, 192, 320, 448, 72, 200, 328, 456, 80, 208, 336, 464, 88, 216, 344, 472 //row 2
-//96, 224, 352, 480, 104, 232, 360, 488, 112, 240, 368, 496, 120, 248, 376, 504 //row 3
 
-//top left, same column-except 4 down, same column-except 4 down,
-
-// Defines 128  x  "8 pixel" chunks (buffer offsets) for each of 4 possible sets of led lines (1024 pixels per line)
-// 16 chunks (8 pixels) per panel, 8 panels
-// Enter numbers from paper lowest value first for each row, IE 67,68,69,70 etc
 unsigned int indexes[4][16]={
 
-					{0,128,256,384,8,136,264,392,16,144,272,400,24,152,280,408}, //row 0
+//{0,128,256,384,8,136,264,392,16,144,272,400,24,152,280,408}, //row 0
+{384,256,128,0,392,264,136,8,400,272,144,16,408,280,152,24},
 
-					{32,160,288,416,40,168,296,424,48,176,304,432,56,184,312,440}, //row 1
+//{32,160,288,416,40,168,296,424,48,176,304,432,56,184,312,440}, //row 1
+{416,288,160,32,424,296,168,40,432,304,176,48,440,312,184,56},
 
-					{64,192,320,448,72,200,328,456,80,208,336,464,88,216,344,472}, //row 2
+//{64,192,320,448,72,200,328,456,80,208,336,464,88,216,344,472}, //row 2
+{448,320,192,64,456,328,200,72,464,336,208,80,472,344,216,88},
 
-					{96,224,352,480,104,232,360,488,112,240,368,496,120,248,376,504}}; //row 3
 
+{480,352,224,96,488,360,232,104,496,368,240,112,504,376,248,120}};
+//{96,224,352,480,104,232,360,488,112,240,368,496,120,248,376,504}}; //row 3
 
