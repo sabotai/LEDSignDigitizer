be61cfb1adf212ff87a0acb5f887b97bf2449661
sabotai <sabotaione@gmail.com>
2013-12-22 00:54:33 -0500

    Update testApp.h

--- videoGrabberExample/src/testApp.h
+++ videoGrabberExample/src/testApp.h
@@ -2,45 +2,61 @@
 
 #include "ofMain.h"
 
-//#include <stdio.h> //SWITCH BACK ON FOR PI
-//#include <wiringPi.h> //SWITCH BACK ON FOR PI
-//#include "ledHashTable.h" //hash table
+#include <stdio.h> 
+#include <wiringPi.h> 
 
 class testApp : public ofBaseApp{
 
-	public:
+public:
 
-		void setup();
-		//void select_row();
-		void update();
-		void draw();
+void setup();
+//void select_row();
+void update();
+void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+void keyPressed(int key);
+void keyReleased(int key);
+void mouseMoved(int x, int y );
+void mouseDragged(int x, int y, int button);
+void mousePressed(int x, int y, int button);
+void mouseReleased(int x, int y, int button);
+void windowResized(int w, int h);
+void dragEvent(ofDragInfo dragInfo);
+void gotMessage(ofMessage msg);
 
-		ofVideoGrabber 		vidGrabber;
-		unsigned char * 	videoInverted;
-		ofTexture			videoTexture;
-		int 				camWidth;
-		int 				camHeight;
-		int                 xPos;
-		int                 yPos;
-		int                 whiteX;
-		int                 whiteY;
-		int                 dotColor;
-		int                 cols;
-		int                 rows;
-		int                 videoScale;
+ofVideoGrabber vidGrabber;
+unsigned char * videoInverted;
+ofTexture	 videoTexture;
+int camWidth;
+int camHeight;
+int                 xPos;
+int                 yPos;
+int                 whiteX;
+int                 whiteY;
+int                 dotColor;
+int                 cols;
+int                 rowsB;
+int                 videoScale;
 
-        unsigned char                 pixelsB[512];
+unsigned char	 pixelsTest[512];
+bool	 pixelsBoolean;
+bool	 start;
+bool	 ascii;
 
-		ofTrueTypeFont myfont;
+       unsigned char       pixelsB[512];
+       unsigned char	 pixelsC[512];
+       
+       int	 counter;
+       int	 threshold;
 
+ofTrueTypeFont myfont;
+
+unsigned char *	 horizontalFlip;
+unsigned char *	 verticalFlip;
+
+
+bool	ballState;
+float	ballX, ballY;
+float	velX, velY;
+int	 ballLoc, oldBallLoc;
 };
